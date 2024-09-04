7ebcb820abb9043d93f03e4a361b784a48975993
Craig Hafer <craighafer96@gmail.com>
2024-09-04 12:00:13 -0400

    Make the mute/unmute buttons slightly better

--- MuteButton.gd
+++ MuteButton.gd
@@ -9,22 +9,21 @@ var unmute = preload("res://unmute.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_parent().get_node("Singleton").muted = Singleton.muted
-	if(get_parent().get_node("Singleton").muted):
+	var singleton = get_parent().get_parent().get_node("Singleton")
+	singleton.muted = Singleton.muted
+
+	if(singleton.muted):
 		texture_normal = unmute
 	else:
 		texture_normal = mute
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_MuteButton_pressed():
+	var singleton = get_parent().get_parent().get_node("Singleton")
+
 	Singleton.muted = !Singleton.muted
-	get_parent().get_node("Singleton").muted = !get_parent().get_node("Singleton").muted
-	if(get_parent().get_node("Singleton").muted):
+	singleton.muted = !singleton.muted
+
+	if(singleton.muted):
 		texture_normal = unmute
 	else:
 		texture_normal = mute
